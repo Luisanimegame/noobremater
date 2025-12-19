b0f9a66d85677226202e47b646fa021321221913
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-12-19 17:18:43 -0300

    colocando video em playstate zzzzz

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1130,7 +1130,8 @@ class PlayState extends MusicBeatState
 				case 'senpai' | 'roses' | 'thorns':
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
-
+				case 'ugh':
+				startVideo('cmr');
 				default:
 					startCountdown();
 			}
